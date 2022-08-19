330a997704337436947c9d3c41da814e213f80ae
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-08-19 08:47:10 -0500

    chore: update Cargo and main()

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use rusty_audio::Audio;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "explode.wav");
+    audio.add("move", "explode.wav");
+    audio.add("pew", "explode.wav");
+    audio.add("startup", "explode.wav");
+    audio.add("win", "explode.wav");
+
+    // Cleanup
+    audio.wait();
 }
